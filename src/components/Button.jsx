dfaffe222a109d0c940866b2925252ce765050f4
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-12-17 04:56:31 +0300

    publishing task added

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import {Button} from "antd";
+import ReactDOM from "react-dom/client";
+
+<ReactDOM>
+<>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+    <Button type="primary" shape="circle"></Button>
+    <Button type="primary" shape="round"></Button>
+    </>
+</ReactDOM>
