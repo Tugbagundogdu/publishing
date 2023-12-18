5109c2be5b35d5c13f13b20628b312734c90c59a
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-12-18 07:09:13 +0300

    publish work fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
-import Button from './components/Button'
+// App.jsx
 
-function App() {
+import Button from './components/Button';
+import './App.css';
+
+const App = () => {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
-    <>
-    <Button />
-    </>
-  )
-}
+    <div className="container">
+      <h1>Welcome to My App</h1>
+      <Button type="primary" onClick={handleClick}>
+        Click Me
+      </Button>
+      <Button type="default" onClick={handleClick}>
+        Default Button
+      </Button>
+      <Button type="dashed" onClick={handleClick}>
+        Dashed Button
+      </Button>
+      <Button type="text" onClick={handleClick}>
+        Text Button
+      </Button>
+      <Button type="link" onClick={handleClick}>
+        Link Button
+      </Button>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,32 @@
-import {Button} from "antd";
-import ReactDOM from "react-dom/client";
+import PropTypes from 'prop-types';
 
-<ReactDOM>
-<>
-    <Button type="primary">Primary Button</Button>
-    <Button>Default Button</Button>
-    <Button type="dashed">Dashed Button</Button>
-    <Button type="text">Text Button</Button>
-    <Button type="link">Link Button</Button>
-    <Button type="primary" shape="circle"></Button>
-    <Button type="primary" shape="round"></Button>
-    </>
-</ReactDOM>
+const Button = ({ type, children, onClick }) => {
+  const buttonTypes = {
+    primary: 'btn-primary',
+    default: 'btn-default',
+    dashed: 'btn-dashed',
+    text: 'btn-text',
+    link: 'btn-link',
+  };
+
+  const buttonClass = `btn ${buttonTypes[type] || 'btn-default'}`;
+
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['primary', 'default', 'dashed', 'text', 'link']),
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  type: 'default',
+  onClick: () => {},
+};
+
+export default Button;
